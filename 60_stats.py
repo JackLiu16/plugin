5efba49fe69682232937074c902e81d55f5b8a49
jackliu <2584316041@qq.com>
2018-07-23 18:17:27 +0800

    fix code

--- 60_stats.py
+++ 60_stats.py
@@ -4,7 +4,7 @@ import time
 import json
 import os
 
-os.system("touch test_plugin")
+os.system("touch /home/work/open-falcon/plugin/test_plugin")
 output = [{"endpoint": "monitor-test-centos", "tags": "", "timestamp": int(time.time()), "metric": "agent.cpu", "value": 1.8, "counterType": "GAUGE", "step": 60}]
 
 print  json.dumps(output)
